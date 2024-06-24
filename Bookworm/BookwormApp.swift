c4a1c4ac6c5636a05253bdb429c33968e3fb6013
IrinaZherebtsova <irinazherebtsova1994@gmail.com>
2024-06-24 11:04:21 -1000

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Irina Zherebtsova on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
